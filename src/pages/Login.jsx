8e558eb2dafcb242febcd9ae4b053534047f3cc7
Desai Priyansh <priyanshdesai62@gmail.com>
2025-05-06 17:25:02 +0530

    minor

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
             <img
               src={companyLogo}
               alt="Logo"
-              className="w-60 h-60 object-cover rounded-lg"
+              className="w-60 xl:w-[28rem] h-[28rem] xl:h-96 object-cover rounded-lg"
             />
           </div>
           <div>
